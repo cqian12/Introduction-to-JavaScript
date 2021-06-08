d898b1c75ea26dafdc8c290d32b3f0f614b1dc18
Charles Qian <charleshqian@gmail.com>
2021-06-08 14:06:10 -0400

    started project, solved task 3

--- index.js
+++ index.js
@@ -107,11 +107,32 @@ Use the hungryDog function and feeding requirements below to do the following:
   NOTE: If done correctly, a weight of 15 lbs and age of 1 year would return 0.44999999999999996
 */  
 
-function hungryDog(/*add your code here*/){
-    /*add your code here*/
+function hungryDog(weight,age){
+    if(age>=1){
+      if(weight<=5){
+        return weight *0.05;
+      }else if(weight >5 && weight <=10){
+        return weight * 0.04;
+      }else if (weight > 10 && weight <=15){
+        return weight * 0.03;
+      }else{
+        return weight * 0.02;
+      }
+    }else {
+      if (age >= 7/12){
+        return weight * 0.04;
+      }else if (age < 7/12 && age >= 4/12) {
+        return weight * 0.05;
+      }else if (age < 4/12 && age >= 2/12) {
+        return weight * 0.1;
+      }else {
+        return 'no dog food for puppies younger than 2 months!';
+      }
+    }
   }
 
-
+console.log('task 3');
+console.log(hungryDog(15,1));
 
 /*🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀*/
 
